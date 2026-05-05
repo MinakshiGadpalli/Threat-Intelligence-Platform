792dd09ced16174d9c13a0898d68a50a4bfe3a01
Minakshi Gadpalli <gadpalliminakshi@gmail.com>
2026-05-04 18:23:45 -0800

    feat: added week 4 dashboard and visualization

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+from db import collection, db
+
+app = Flask(__name__)
+
+alerts_collection = db["alerts"]
+
+@app.route("/")
+def dashboard():
+    threats = list(collection.find({}, {"_id": 0}))
+    alerts = list(alerts_collection.find({}, {"_id": 0}))
+    return render_template("dashboard.html", threats=threats, alerts=alerts)
+
+if __name__ == "__main__":
+    app.run(debug=True)
